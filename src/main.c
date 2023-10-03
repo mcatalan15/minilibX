62a5344d284e7a7abcc941aade5a7d8d840535bf
Marc Catalan Sanchez <mcatalan@student.42barcelona.com>
2023-10-03 14:52:05 +0200

    adding image code

--- src/main.c
+++ src/main.c
@@ -6,48 +6,31 @@
 /*   By: mcatalan <mcatalan@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 12:36:28 by mcatalan          #+#    #+#             */
-/*   Updated: 2023/10/02 16:05:37 by mcatalan         ###   ########.fr       */
+/*   Updated: 2023/10/03 14:45:13 by mcatalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../includes/so_long.h"
 
-int	main(void)
-{
-	void	*mlx;
-	void	*mlx_win;
-
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	mlx_loop(mlx);
-	mlx_loop(mlx_win);
-}
-
-
-// typedef struct s_data {
-// 	void	*img;
-// 	char	*addr;
-// 	int		bits_per_pixel;
-// 	int		line_length;
-// 	int		endian;
-// }				t_data;
+/*
+Initial function to create a new empty window in size 1920 x 1080. Also adding the detail "Hello world!"
+to the name of the window.
+*/
 
 // int	main(void)
 // {
 // 	void	*mlx;
-// 	t_data	img;
+// 	void	*mlx_win;
 
 // 	mlx = mlx_init();
-// 	img.img = mlx_new_image(mlx, 1920, 1080);
-
-// 	/*
-// 	** After creating an image, we can call `mlx_get_data_addr`, we pass
-// 	** `bits_per_pixel`, `line_length`, and `endian` by reference. These will
-// 	** then be set accordingly for the *current* data address.
-// 	*/
-// 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-// 									&img.endian);
+// 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+// 	mlx_loop(mlx);
+// 	mlx_loop(mlx_win);
 // }
+
+/*
+Initial Struct to write pixels
+*/
 // typedef struct s_data {
 // 	void	*img;
 // 	char	*addr;
@@ -55,6 +38,22 @@ int	main(void)
 // 	int		line_length;
 // 	int		endian;
 // }				t_data;
+
+/*
+function to add color and write the pixel in the screen
+*/
+
+// void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+// {
+// 	char	*dst;
+
+// 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+// 	*(unsigned int*)dst = color;
+// }
+
+/*
+Main function to execute the window and create and image with the pixel declarated and showing the image
+*/
 
 // int	main(void)
 // {
@@ -66,7 +65,19 @@ int	main(void)
 // 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
 // 	img.img = mlx_new_image(mlx, 1920, 1080);
 // 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-// 	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+// 	my_mlx_pixel_put(&img, 450, 890, 0x00FF0000);
 // 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 // 	mlx_loop(mlx);
 // }
+
+int	main(void)
+{
+	void	*mlx;
+	void	*img;
+	char	*relative_path = "./test.xpm";
+	int		img_width;
+	int		img_height;
+
+	mlx = mlx_init();
+	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
+}
